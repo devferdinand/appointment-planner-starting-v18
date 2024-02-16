388c88c724f001a3a348bce13e82213459b8ce6c
Ferdinand Lukito <devferdinandlukito@gmail.com>
2024-02-16 14:55:13 -0500

    fifth commit
    Added to TileList to receive prop and render Tile components and pass props to Tile. In Tile, renders data from props passed to the page.

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,13 @@
 import React from "react";
+import {Tile} from "../tile/Tile"
 
-export const TileList = () => {
+export const TileList = ({contacts}) => {
   return (
     <div>
-      
+      {contacts.map((contact, index) => { // traverse through every contact in contacts
+        const { name, ...description } = contact; // restructuring syntax
+        return <Tile name={name} description={description} key={index} />
+      })}
     </div>
   );
 };
