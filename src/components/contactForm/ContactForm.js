aa1cf5f2594ef85a25298218294b4970545f96d0
Ferdinand Lukito <devferdinandlukito@gmail.com>
2024-02-16 14:03:10 -0500

    fourth commit
    Added form in ContactForm with three controlled input and a submit button.

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,16 +1,15 @@
 import React from "react";
 
-export const ContactForm = ({
-  name,
-  setName,
-  phone,
-  setPhone,
-  email,
-  setEmail,
-  handleSubmit
-}) => {
+export const ContactForm = ({name, setName, phone, setPhone, email, setEmail, handleSubmit}) => {
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={name} placeholder="Contact Name (Firstname Lastname)" required onChange={(e) => setName(e.target.value)}></input>
+        <input type='tel' value={phone} pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Contact Phone (xxx-xxx-xxxx)" required onChange={(e) => setPhone(e.target.value)}></input>
+        <input type='email' value={email} placeholder="Contact Email (user@gmail.com)" required onChange={(e) => setEmail(e.target.value)}></input>
+        <input type="submit" value="Add Data To Contacts"></input>
+      </form>
+    </>
   );
 };
 
